e785c2af68dba504380928b81025e97eb303729f
aleksaa13 <albijanic.a13@gmail.com>
2020-04-02 17:58:21 +0200

    some details fixed at task1

--- task1/index.js
+++ task1/index.js
@@ -1,84 +1,103 @@
-var income=0;
-var expenses=0;
+var income = 0;
+var expenses = 0;
 let storageObj = {
-    income:0,
-    expenses:0,
-    incList:{incListNames:[],
-               incListValues:[]
-            },
-    expList:{expListNames:[],
-            expListValues:[],
-            expListPercent:[]
-     }
+    income: 0,
+    expenses: 0,
+    incList: {
+        incListNames: [],
+        incListValues: []
+    },
+    expList: {
+        expListNames: [],
+        expListValues: [],
+        expListPercent: []
+    }
 };
 
-window.addEventListener("load",unloadStorage);
-function unloadStorage(){
-    let storage=JSON.parse(localStorage.getItem("values"));
+window.addEventListener("load", unloadStorage);
+function unloadStorage() {
+    let storage = JSON.parse(localStorage.getItem("values"));
     console.log(storage);
-    let list=document.getElementById("inc");
-    for(let i=0;i<storage.incList.incListNames.length;i++){
-        let description=storage.incList.incListNames[i];
-        let value=storage.incList.incListValues[i];
-        
-            let li = document.createElement("li");
-            let par2 = document.createElement("p");
-            let button = document.createElement("button");
-            button.appendChild(document.createTextNode("X"));
-            button.classList.add("deleteBtn");
-            let par1 = document.createElement("p");
-            par1.textContent = description;
-            par1.classList.add("li-par-left");
-            par2.classList.add("li-par-right");
-            par2.classList.add("green");
-            let floatValue = parseFloat(value).toFixed(2);
-            par2.appendChild(document.createTextNode(value));
-            li.appendChild(par1);
-            li.appendChild(button);
-            li.appendChild(par2);
-            list.appendChild(li);
-    }
-    
-    list=document.getElementById("exp");
-    for(let i=0;i<storage.expList.expListNames.length;i++){
-        description=storage.expList.expListNames[i];
-        value=storage.expList.expListValues[i];
-        let percent=storage.expList.expListPercent[i];
+    let list = document.getElementById("inc");
+    for (let i = 0; i < storage.incList.incListNames.length; i++) {
+        let description = storage.incList.incListNames[i];
+        let value = storage.incList.incListValues[i];
+
         let li = document.createElement("li");
-            let par2 = document.createElement("p");
-            let button = document.createElement("button");
-            button.appendChild(document.createTextNode("X"));
-            button.classList.add("deleteBtn");
-            let par1 = document.createElement("p");
-            par1.textContent = description;
-            par1.classList.add("li-par-left");
-            par2.classList.add("li-par-right");
-            par2.classList.add("red");
-            let floatValue = parseFloat(value).toFixed(2);
-            par2.appendChild(document.createTextNode(value));
-            let par3 = document.createElement("p");
-            par3.classList.add("par-percent");
-            par3.appendChild(document.createTextNode(percent));
-             li.appendChild(par3);
-             li.appendChild(par1);
-             li.appendChild(button);
-             li.appendChild(par2);
-             list.appendChild(li);
+        let par2 = document.createElement("p");
+        let button = document.createElement("button");
+        button.appendChild(document.createTextNode("X"));
+        button.classList.add("deleteBtn");
+        let par1 = document.createElement("p");
+        par1.textContent = description;
+        par1.classList.add("li-par-left");
+        par2.classList.add("li-par-right");
+        par2.classList.add("green");
+        let floatValue = parseFloat(value).toFixed(2);
+        par2.appendChild(document.createTextNode(value));
+        li.appendChild(par1);
+        li.appendChild(button);
+        li.appendChild(par2);
+        list.appendChild(li);
+    }
+
+    list = document.getElementById("exp");
+    for (let i = 0; i < storage.expList.expListNames.length; i++) {
+        description = storage.expList.expListNames[i];
+        value = storage.expList.expListValues[i];
+        let percent = storage.expList.expListPercent[i];
+        let li = document.createElement("li");
+        let par2 = document.createElement("p");
+        let button = document.createElement("button");
+        button.appendChild(document.createTextNode("X"));
+        button.classList.add("deleteBtn");
+        let par1 = document.createElement("p");
+        par1.textContent = description;
+        par1.classList.add("li-par-left");
+        par2.classList.add("li-par-right");
+        par2.classList.add("red");
+        let floatValue = parseFloat(value).toFixed(2);
+        par2.appendChild(document.createTextNode(value));
+        let par3 = document.createElement("p");
+        par3.classList.add("par-percent");
+        par3.appendChild(document.createTextNode(percent));
+        li.appendChild(par3);
+        li.appendChild(par1);
+        li.appendChild(button);
+        li.appendChild(par2);
+        list.appendChild(li);
+    }
+
+    income = storage.income;
+    expenses = storage.expenses;
+    document.getElementById("income").textContent = "+" + income.toFixed(2);
+    if (expenses === 0) {
+        document.getElementById("expenses").textContent = "-" + expenses.toFixed(2);
+    }
+    else {
+        document.getElementById("expenses").textContent = expenses.toFixed(2);
+    }
+    if ((income + expenses).toFixed(2) > 0) {
+        document.getElementsByClassName("amount")[0].textContent = "+" + (income + expenses).toFixed(2);
+    }
+    if (income === expenses) {
+        document.getElementsByClassName("amount")[0].textContent = "0.00";
+    }
+    else {
+        document.getElementsByClassName("amount")[0].textContent = (income + expenses).toFixed(2);
+    }
+    let totalPercentage = expenses / income * 100 * (-1);
+    document.getElementsByClassName("span3")[0].textContent = Math.ceil(totalPercentage) + "%";
+
+    if (expenses === 0) {
+        document.getElementsByClassName("span3")[0].style.display = "none";
+    }
+    else {
+        document.getElementsByClassName("span3")[0].style.display = "block";
+    }
 }
 
-income=storage.income;
-expenses=storage.expenses;
-document.getElementById("income").textContent = "+" + income.toFixed(2);
-document.getElementById("expenses").textContent = expenses.toFixed(2);
-if ((income + expenses).toFixed(2) > 0) {
-    document.getElementsByClassName("amount")[0].textContent = "+" + (income + expenses).toFixed(2);
-}
-else {
-    document.getElementsByClassName("amount")[0].textContent = (income + expenses).toFixed(2);
-}
-let totalPercentage = expenses / income * 100 * (-1);
-            document.getElementsByClassName("span3")[0].textContent = Math.ceil(totalPercentage) + "%";
-}
+
 
 
 function writeMonth() {
@@ -140,6 +159,12 @@ function addItem() {
                 par3.appendChild(document.createTextNode(Math.round(percentage) + "%"));
                 li.appendChild(par3);
                 expenses = expenses - parseFloat(floatValue);
+                if (expenses === 0) {
+                    document.getElementsByClassName("span3")[0].style.display = "none";
+                }
+                else {
+                    document.getElementsByClassName("span3")[0].style.display = "block";
+                }
             }
             else {
                 par2.classList.add("green");
@@ -204,6 +229,15 @@ function deleteItem(event) {
         let totalPercentage = (expenses / income * 100 * (-1).toFixed(0));
         document.getElementsByClassName("span3")[0].textContent = Math.ceil(totalPercentage) + "%";
     }
+    if (document.getElementById("expenses").textContent === "-0.00") {
+        document.getElementsByClassName("span3")[0].style.display = "none";
+    }
+    if (expenses === 0) {
+        document.getElementsByClassName("span3")[0].style.display = "none";
+    }
+    else {
+        document.getElementsByClassName("span3")[0].style.display = "block";
+    }
 }
 document.getElementById("btn").addEventListener("click", allowOnlyNumbers);
 //BLOKIRA UNOS SLOVA U VALUE POLJE
@@ -217,25 +251,25 @@ function allowOnlyNumbers() {
         return false;
     }
 }
-window.addEventListener("unload",loadStorage);
-function loadStorage(){
-    let incomeList=document.getElementById("inc");
-    let names=incomeList.getElementsByClassName("li-par-left");
-    let values=incomeList.getElementsByClassName("li-par-right");
-    for(let i=0;i<names.length;i++){
+window.addEventListener("unload", loadStorage);
+function loadStorage() {
+    let incomeList = document.getElementById("inc");
+    let names = incomeList.getElementsByClassName("li-par-left");
+    let values = incomeList.getElementsByClassName("li-par-right");
+    for (let i = 0; i < names.length; i++) {
         storageObj.incList.incListNames.push(names[i].textContent);
         storageObj.incList.incListValues.push(values[i].textContent);
     }
-    let expensesList=document.getElementById("exp");    
-    let names1=expensesList.getElementsByClassName("li-par-left");
-    let values1=expensesList.getElementsByClassName("li-par-right");
-    let percent=expensesList.getElementsByClassName("par-percent");
-    for(let i=0;i<names1.length;i++){
+    let expensesList = document.getElementById("exp");
+    let names1 = expensesList.getElementsByClassName("li-par-left");
+    let values1 = expensesList.getElementsByClassName("li-par-right");
+    let percent = expensesList.getElementsByClassName("par-percent");
+    for (let i = 0; i < names1.length; i++) {
         storageObj.expList.expListNames.push(names1[i].textContent);
         storageObj.expList.expListValues.push(values1[i].textContent);
         storageObj.expList.expListPercent.push(percent[i].textContent);
     }
-    storageObj.income=income;
-    storageObj.expenses=expenses;
-    localStorage.setItem("values",JSON.stringify(storageObj));
+    storageObj.income = income;
+    storageObj.expenses = expenses;
+    localStorage.setItem("values", JSON.stringify(storageObj));
 }
