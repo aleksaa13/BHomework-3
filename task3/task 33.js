a1103210e8e0ed33becde4361b7fa9027972cf09
aleksaa13 <albijanic.a13@gmail.com>
2020-04-02 17:27:27 +0200

    iterators challenges done

---
+++ task3/task 33.js
@@ -0,0 +1,203 @@
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+//console.log('Hello, world!');
+
+// CHALLENGE 1
+
+function sumFunc(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    return sum;
+  
+  }
+  
+  // Uncomment the lines below to test your work
+  // const array = [1, 2, 3, 4];
+  // console.log(sumFunc(array)); // -> should log 10
+  
+  function returnIterator(arr) {
+    let b=0;
+    return function(){
+      let a=arr[b];
+      return a;
+      a++;
+    }
+  
+  }
+  
+  // Uncomment the lines below to test your work
+  // const array2 = ['a', 'b', 'c', 'd'];
+  // const myIterator = returnIterator(array2);
+  // console.log(myIterator()); // -> should log 'a'
+  // console.log(myIterator()); // -> should log 'b'
+  // console.log(myIterator()); // -> should log 'c'
+  // console.log(myIterator()); // -> should log 'd'
+  
+  
+  
+  // CHALLENGE 2
+  
+  function nextIterator(arr) {
+    let count = 0;
+          const next = {
+                  next() {
+                          return arr[count++];
+                  },
+          };
+          
+          return next;
+  
+  }
+  
+  // Uncomment the lines below to test your work
+  //const array3 = [1, 2, 3];
+  // const iteratorWithNext = nextIterator(array3);
+  // console.log(iteratorWithNext.next()); // -> should log 1
+  // console.log(iteratorWithNext.next()); // -> should log 2
+  // console.log(iteratorWithNext.next()); // -> should log 3
+  
+  
+  
+  // CHALLENGE 3
+  
+  function sumArray(arr) {
+    let sum = 0;
+          const iterator = nextIterator(arr);
+  
+          for (const index in arr) {
+                  sum += iterator.next();
+          }
+          return sum;
+  
+  }
+  
+  // Uncomment the lines below to test your work
+  // const array4 = [1, 2, 3, 4];
+  // console.log(sumArray(array4)); // -> should log 10
+  
+  
+  
+  // CHALLENGE 4
+  
+  function setIterator(set) {
+   const entries = [...set];
+          let count = 0;
+          return {
+                  next() {
+                          return entries[count++];
+                  },
+          };
+  
+  }
+  
+  // Uncomment the lines below to test your work
+  // const mySet = new Set('hey');
+  // const iterateSet = setIterator(mySet);
+  // console.log(iterateSet.next()); // -> should log 'h'
+  // console.log(iterateSet.next()); // -> should log 'e'
+  // console.log(iterateSet.next()); // -> should log 'y'
+  
+  
+  
+  // CHALLENGE 5
+  
+  function indexIterator(arr) {
+    let count = 0;
+          const array = Object.entries(arr);
+  
+          return {
+                  next() {
+                          return array[count++];
+                  },
+          };
+  
+  }
+  
+  // Uncomment the lines below to test your work
+  // const array5 = ['a', 'b', 'c', 'd'];
+  // const iteratorWithIndex = indexIterator(array5);
+  // console.log(iteratorWithIndex.next()); // -> should log [0, 'a']
+  // console.log(iteratorWithIndex.next()); // -> should log [1, 'b']
+  // console.log(iteratorWithIndex.next()); // -> should log [2, 'c']
+  
+  
+  
+  // CHALLENGE 6
+  
+  function Words(string) {
+    this.str = string;
+    return this.str.split(" ");
+  }
+  
+  Words.prototype[Symbol.iterator] = function() {
+    // YOUR CODE HERE
+    let i = 0;
+    return {
+      next: function() {
+        let elem = this[i];
+        i++;
+        return elem;
+      }
+    };
+  };
+  
+  // Uncomment the lines below to test your work
+  const helloWorld = new Words("Hello World");
+  for (let word of helloWorld) {
+    console.log(word);
+  } // -> should log 'Hello' and 'World'd'
+  
+  // CHALLENGE 7
+  
+  function valueAndPrevIndex(array){
+  let count = 0;
+  
+          return {
+                  sentence() {
+                          return count++ === 0 ? "was found after index first" : `was found after index ${count - 1}`;
+                  },
+          };
+  }
+  
+  const returnedSentence = valueAndPrevIndex([4,5,6])
+  //console.log(returnedSentence.sentence());
+  //console.log(returnedSentence.sentence());
+  //console.log(returnedSentence.sentence());
+  
+  
+  //CHALLENGE 8
+  
+  function* createConversation(string) {
+  yield setInterval(function() {
+      if (str == "english") {
+        console.log("hello there");
+      } else {
+        console.log("gibberish");
+      }
+    }, 3000);
+  
+  }
+  
+  //console.log(createConversation('english').next());
+  
+  
+  
+  //CHALLENGE 9
+  function waitForVerb(noun) {
+  return `${noun} plays`;
+  }
+  
+  async function f(noun) {
+  const concat = await new Promise(function(resolve) {
+                  return setTimeout(function() {
+                          resolve(waitForVerb(noun));
+                  }, 3000);
+          });
+  
+          return console.log(concat);
+  
+  }
+  
+  f("dog");
+    
